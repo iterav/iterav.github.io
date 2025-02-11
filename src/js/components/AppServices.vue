c52cc24186b531177d0bb38950237bee94d41fd6
Sean Delaney <hello@delaneymethod.com>
2025-02-11 11:14:25 +0000

    fix: services icon

--- src/js/components/AppServices.vue
+++ src/js/components/AppServices.vue
@@ -1,4 +1,5 @@
-<script setup lang="js"></script>
+<script setup lang="js">
+</script>
 
 <template>
 	<section id="services" class="flex items-center justify-center py-20  bg-linear-to-b/shorter from-slate-800 from-50% to-slate-950 to-100%">
@@ -53,14 +54,19 @@
 								</div>
 								<div class="w-fit">Surround Sound Systems</div>
 							</li>
-							<li class="flex flex-row items-center justify-start gap-2">
-								<div class="w-7 pt-0.5">
+							<li class="flex flex-row items-start justify-start gap-2">
+								<div class="w-7 pt-1">
 									<svg class="w-6" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 										<path d="M2 7.8C2 6.11984 2 5.27976 2.32698 4.63803C2.6146 4.07354 3.07354 3.6146 3.63803 3.32698C4.27976 3 5.11984 3 6.8 3H17.2C18.8802 3 19.7202 3 20.362 3.32698C20.9265 3.6146 21.3854 4.07354 21.673 4.63803C22 5.27976 22 6.11984 22 7.8V16.2C22 17.8802 22 18.7202 21.673 19.362C21.3854 19.9265 20.9265 20.3854 20.362 20.673C19.7202 21 18.8802 21 17.2 21H6.8C5.11984 21 4.27976 21 3.63803 20.673C3.07354 20.3854 2.6146 19.9265 2.32698 19.362C2 18.7202 2 17.8802 2 16.2V7.8Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
 										<path d="M12 16C14.2091 16 16 14.2091 16 12C16 9.79086 14.2091 8 12 8C9.79086 8 8 9.79086 8 12C8 14.2091 9.79086 16 12 16Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
 									</svg>
 								</div>
-								<div class="w-fit">Cameras (Internal and External)</div>
+								<div class="w-fit">
+                                    <div class="flex flex-col">
+                                        Cameras
+                                        <span class="text-sm leading-6">Internal and External</span>
+                                    </div>
+                                </div>
 							</li>
 							<li class="flex flex-row items-center justify-start gap-2">
 								<div class="w-7 pt-0.5">
